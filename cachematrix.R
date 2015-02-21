24ce65c622585d06b0e3e75f82582d173b4703b2
AnjuK <aig.anju@gmail.com>
2015-02-21 16:35:13 -0600

    Cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# I Anju Kamra wrote this code
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+AnjuCacheMatrix <- function(x = matrix()) {
+        i <- NULL ## creating a blank matrix to store Inverse
+        set <- function(y) {
+                x <<- y  ## Assigning new matrix to x using set
+                i <<- NULL
+        }
+        get <- function() x
+        ##Storing the inverse in i after it is calculated
+        setsolve <- function(solve) i <<- solve 
+        
+        ##Caching value from cache if available
+        getsolve <- function() i 
+        
+        ##Store the values in a list and return the list
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## This function calculates the inverse of a matrix. If cached value of inverse is available it gets
+## it from there else it calculates
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getsolve() #Check if matrix x is cached
+        if(!is.null(i)) {
+                message("getting cached data") #If the matrix x is cached than get the cached value
+                return(i)
+        }
+        data <- x$get() # We get it If matrix is not cached and caclculate the inverse
+        i <- solve(data, ...) 
+        x$setsolve(i) #Caching the Inverse of the matrix that was created above
+        i
 }
+
+##Test Cases
+x <- makeCacheMatrix(matrix(1:4,2))
+z<-x$get() ##Get the matrix created
+z
+
+cacheSolve(x) ## Getting the inverse
+cacheSolve(x) ## Checking if cached inverse is returned
+
+#Testing if inverse is correct
+y<-x$getsolve()
+y %*% z ## Should z be identity matrix
